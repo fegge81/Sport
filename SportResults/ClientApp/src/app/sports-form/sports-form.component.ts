43b6de63150fff0afab6eeb7b060dabe70d14c3b
fredrik emanuelsson <fredrik.emanuelsson81@gmial.com>
2019-09-14 20:21:19 +0100

    Add project files.

---
+++ SportResults/ClientApp/src/app/sports-form/sports-form.component.ts
@@ -0,0 +1,22 @@
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+    selector: 'app-sports-form',
+    templateUrl: './sports-form.component.html',
+})
+/** sports-form component*/
+export class SportsFormComponent {
+  //public sportform: SportForm[]
+
+  //constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+  //  http.get<SportForm[]>(baseUrl + 'api/SampleData/SportForm').subscribe(result => {
+  //    this.sportform = result;
+  //  }, error => console.error(error));
+  //}
+}
+
+
+//interface SportForm {
+ 
+//}
